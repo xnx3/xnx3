953d91ad8a7a83569e9863862317a929b9131dd0
xnx3 <mail@xnx3.com>
2017-03-24 17:06:44 +0800

    xnx3
    DelayCycleExecuteUtil.setSleepArray 注释补全

--- src/com/xnx3/DelayCycleExecuteUtil.java
+++ src/com/xnx3/DelayCycleExecuteUtil.java
@@ -28,7 +28,7 @@ public class DelayCycleExecuteUtil extends Thread {
 	/**
 	 * 设置每次执行的延迟时间，单位：毫秒。
 	 * <br/>如果执行 {@link DelayCycleExecute#executeProcedures(int)} 失败，则延迟指定时间后，继续执行。直到成功为止，才退出循环
-	 * <br/><br/>如设定数组为：{0,2000,6000,20000}
+	 * <br/><br/>如设定数组为：new int[]{0,2000,6000,20000}
 	 * <br/>则第一次执行时，立即执行，不延迟；
 	 * <br/>若失败，则延迟2秒后再次执行；
 	 * <br/>若还失败，则延迟6秒后再次执行；
@@ -36,7 +36,7 @@ public class DelayCycleExecuteUtil extends Thread {
 	 * <br/>若还失败，则延迟20秒后再次执行；
 	 * <br/>若还失败，此此时数组已到末尾，结束线程。同时会运行 {@link DelayCycleExecute#failure()} 方法
 	 * <br/><br/>若不设置，默认为：
-	 * <br/>{0,3000,10000,60000,600000,6000000}
+	 * <br/>new int[]{0,3000,10000,60000,600000,6000000}
 	 * @param sleepArray 设定的延迟数组。
 	 */
 	public void setSleepArray(int[] sleepArray){
