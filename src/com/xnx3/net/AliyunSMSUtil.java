d7993759082678b60d2fe194c994821b8ed2ffa2
xnx3 <mail@xnx3.com>
2018-01-21 21:51:59 +0800

    更新阿里云短信发送
    阿里云短信通道更新，更新了jar包及工具类。但工具类的使用方式不变，兼容之前的发送方式

--- src/com/xnx3/net/AliyunSMSUtil.java
+++ src/com/xnx3/net/AliyunSMSUtil.java
@@ -2,19 +2,19 @@ package com.xnx3.net;
 
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
 import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.exceptions.ServerException;
 import com.aliyuncs.profile.DefaultProfile;
 import com.aliyuncs.profile.IClientProfile;
-import com.aliyuncs.sms.model.v20160927.SingleSendSmsRequest;
-import com.aliyuncs.sms.model.v20160927.SingleSendSmsResponse;
 import com.xnx3.BaseVO;
 
 /**
  * 阿里云短信发送
  * 需Jar包：
  * <br/>aliyun-java-sdk-core-2.4.3.jar
- * <br/>aliyun-java-sdk-sms-3.0.0-rc1.jar
+ * <br/>aliyun-java-sdk-dysmsapi-1.0.0.jar
  * @author 管雷鸣
  * @see https://help.aliyun.com/document_detail/44364.html
  */
@@ -23,11 +23,8 @@ public class AliyunSMSUtil {
 	public String accessKeyId;
 	public String accessKeySecret;
 	
-	/*短信具体每条发送的相关参数*/
-	public String signName;
-	public String templateCode;
-	public String paramString;
-	public String RecNum;
+	static final String product = "Dysmsapi";	//产品名称:云通信短信API产品,开发者无需替换
+    static final String domain = "dysmsapi.aliyuncs.com";	//产品域名,开发者无需替换
 	
 	/**
 	 * 阿里云短信发送配置参数初始化
@@ -46,36 +43,8 @@ public class AliyunSMSUtil {
 		this.accessKeySecret = accessKeySecret;
 	}
 	
-	/**
-	 * 设置发送短信的参数。设置完毕后可以直接通过 {@link #send()}发送
-	 * <br/>等同于直接调用 {@link #send(String, String, String, String)}进行发送
-	 * 	<pre>sms.send("网市场","SMS_40000000","{\"code\":\"123456\"}","18711111111");</pre>
-	 * @param signName 控制台创建的签名名称（状态必须是验证通过）
-	 * 				<br/>&nbsp;&nbsp;&nbsp;&nbsp; https://sms.console.aliyun.com/?spm=#/sms/Sign
-	 * @param templateCode 控制台创建的模板CODE（状态必须是验证通过）
-	 * 				<br/>&nbsp;&nbsp;&nbsp;&nbsp; https://sms.console.aliyun.com/?spm=#/sms/Template
-	 * @param paramString 短信模板中的变量；数字需要转换为字符串；个人用户每个变量长度必须小于15个字符。 例如:短信模板为：“接受短信验证码${no}”,此参数传递{“no”:”123456”}，用户将接收到[短信签名]接受短信验证码123456，传入的字符串为JSON格式
-	 * @param RecNum 目标手机号，多个手机号可以逗号分隔
-	 */
-	public void setSendParam(String signName,String templateCode,String paramString, String RecNum){
-		
-	}
-	
-	/**
-	 * 发送短信
-	 * <br/><b>前提是已经设置了要发送短信的相关参数 {@link #setSendParam(String, String, String, String)}</b>
-	 * <br/>等同于直接调用 {@link #send(String, String, String, String)}进行发送
-	 * @return {@link BaseVO} 
-	 * 			<ul>
-	 * 				<li>若成功，返回 {@link BaseVO#SUCCESS}，此时可以用 {@link BaseVO#getInfo()} 拿到其requestId</li>
-	 * 				<li>若失败，返回 {@link BaseVO#FAILURE}，此时可以用 {@link BaseVO#getInfo()} 拿到其错误原因(catch的抛出的异常名字)</li>
-	 * 			</ul>
-	 */
-	public BaseVO send() {
-		return null;
-	}
-	
-	/**
+
+    /**
 	 * 发送短信，如
 	 * 	<pre>sms.send("网市场","SMS_40000000","{\"code\":\"123456\"}","18711111111");</pre>
 	 * @param signName 控制台创建的签名名称（状态必须是验证通过）
@@ -83,46 +52,65 @@ public class AliyunSMSUtil {
 	 * @param templateCode 控制台创建的模板CODE（状态必须是验证通过）
 	 * 				<br/>&nbsp;&nbsp;&nbsp;&nbsp; https://sms.console.aliyun.com/?spm=#/sms/Template
 	 * @param paramString 短信模板中的变量；数字需要转换为字符串；个人用户每个变量长度必须小于15个字符。 例如:短信模板为：“接受短信验证码${no}”,此参数传递{“no”:”123456”}，用户将接收到[短信签名]接受短信验证码123456，传入的字符串为JSON格式
-	 * @param RecNum 目标手机号，多个手机号可以逗号分隔
+	 * @param phone 接收短信的手机号
 	 * @return {@link BaseVO} 
 	 * 			<ul>
 	 * 				<li>若成功，返回 {@link BaseVO#SUCCESS}，此时可以用 {@link BaseVO#getInfo()} 拿到其requestId</li>
 	 * 				<li>若失败，返回 {@link BaseVO#FAILURE}，此时可以用 {@link BaseVO#getInfo()} 拿到其错误原因(catch的抛出的异常名字)</li>
 	 * 			</ul>
 	 */
-	public BaseVO send(String signName,String templateCode,String paramString, String RecNum) {
-		BaseVO vo = new BaseVO();
-        IClientProfile profile = DefaultProfile.getProfile(this.regionId,this.accessKeyId,this.accessKeySecret);
+    public BaseVO send(String signName,String templateCode,String templateParamString, String phone) {
+    	BaseVO vo = new BaseVO();
+    	
+        //可自助调整超时时间
+        System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
+        System.setProperty("sun.net.client.defaultReadTimeout", "10000");
+
+        //初始化acsClient,暂不支持region化
+        IClientProfile profile = DefaultProfile.getProfile(regionId, accessKeyId, accessKeySecret);
         try {
-			DefaultProfile.addEndpoint(this.regionId, this.regionId, "Sms",  "sms.aliyuncs.com");
+			DefaultProfile.addEndpoint(regionId, regionId, product, domain);
 		} catch (ClientException e1) {
 			e1.printStackTrace();
 			vo.setBaseVO(BaseVO.FAILURE, e1.getMessage());
 			return vo;
 		}
-        IAcsClient client = new DefaultAcsClient(profile);
-        SingleSendSmsRequest request = new SingleSendSmsRequest();
+        IAcsClient acsClient = new DefaultAcsClient(profile);
+
+        //组装请求对象-具体描述见控制台-文档部分内容
+        SendSmsRequest request = new SendSmsRequest();
+        //必填:待发送手机号
+        request.setPhoneNumbers(phone);
+        //必填:短信签名-可在短信控制台中找到
+        request.setSignName(signName);
+        //必填:短信模板-可在短信控制台中找到
+        request.setTemplateCode(templateCode);
+        //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
+        request.setTemplateParam(templateParamString);
+
+        //选填-上行短信扩展码(无特殊需求用户请忽略此字段)
+        //request.setSmsUpExtendCode("90997");
+
+        //可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
+//        request.setOutId("yourOutId");
+
+        //hint 此处可能会抛出异常，注意catch
         try {
-            request.setSignName(signName);	//控制台创建的签名名称
-            request.setTemplateCode(templateCode);//控制台创建的模板CODE
-            request.setParamString(paramString);	//json格式的变量
-            request.setRecNum(RecNum);			//发送至的手机号
-            SingleSendSmsResponse httpResponse = client.getAcsResponse(request);
-            vo.setBaseVO(BaseVO.SUCCESS, httpResponse.getRequestId());
-			return vo;
-        } catch (ServerException e) {
-            e.printStackTrace();
-            vo.setBaseVO(BaseVO.FAILURE, e.getMessage());
-			return vo;
-        }catch (ClientException e) {
-            e.printStackTrace();
-            vo.setBaseVO(BaseVO.FAILURE, e.getMessage());
-			return vo;
-        }
+			SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);
+			vo.setInfo(sendSmsResponse.getRequestId());
+		} catch (ServerException e) {
+			vo.setBaseVO(BaseVO.FAILURE, e.getMessage());
+			e.printStackTrace();
+		} catch (ClientException e) {
+			vo.setBaseVO(BaseVO.FAILURE, e.getMessage());
+			e.printStackTrace();
+		}
+        
+        return vo;
     }
 	
 	public static void main(String[] args) {
 		AliyunSMSUtil sms = new AliyunSMSUtil("cn-hongkong", "...", "...");
-		sms.send("网市场","SMS_44315000","{\"code\":\"12345s6\"}","17076012262");
+		sms.send("网市场","SMS_1234566","{\"code\":\"12345s6\"}","18788888888");
 	}
 }
