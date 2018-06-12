02100bb34ce2670f56a0dda29f93b111dcc3c7c6
xnx3 <mail@xnx3.com>
2018-06-12 14:38:52 +0800

    update

--- src/com/xnx3/j2ee/util/TerminalDetection.java
+++ src/com/xnx3/j2ee/util/TerminalDetection.java
@@ -40,6 +40,42 @@ public class TerminalDetection {
     	}
     	
         return isMobile;  
-    }  
+    }
+    
+
+	/** 
+     * 检测是PC端还是手机端访问，先判断PC端，若系统是 Windows或者Max OSX或者Ubuntu，则返回false，其他的都返回true
+     * @param userAgent 浏览器的 user-agent 字符串
+     * @return true:移动设备接入，false:pc端接入 
+     */  
+    public static boolean checkMobileOrPc(String userAgent){    
+    	String userAgents = userAgent.toLowerCase();
+    	boolean isMobile = true;
+    	if(userAgents == null){
+    		return true;
+    	}
+    	
+    	UserAgent ua = UserAgent.parseUserAgentString(userAgents); 
+    	if(ua == null){
+    		return true;
+    	}
+    	String systemName = ua.getOperatingSystem().getName();
+    	if(systemName == null){
+    		return true;
+    	}
+    	for(int i=0;i<PC_SYSTEMS.length;i++){
+    		if(systemName.equals(PC_SYSTEMS[i])){
+    			return false;
+    		}
+    	}
+    	
+        return isMobile;  
+    }
+    
+    
+    public static void main(String[] args) {
+    	String ua = "Mozilla/5.0 (Linux; Android 5.1; vivo X6D Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.132 MQQBrowser/6.2 TBS/043906 Mobile Safari/537.36 MicroMessenger/6.6.3.1260(0x26060339) NetType/4G Language/zh_CN";
+		System.out.println(checkMobileOrPc(ua));
+	}
 	
 }
